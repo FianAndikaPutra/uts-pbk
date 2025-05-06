0423165a433c3b7c3e3fe82668b0ea238f4f1530
FianAndikaPutra <fianandikaputra@student.uir.ac.id>
2025-05-06 14:12:57 +0700

    Menambahkan kolom input dan button

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
   <div>
     <h1>LIST KEGIATAN</h1>
+
+    <div>
+      <input v-model="newTask" placeholder="Tulis Kegiatan Mu..." />
+      <button @click="addTask">Tambah</button>
+    </div>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 
+const newTask = ref('')
+
+function addTask() {
+  console.log('Tambah:', newTask.value)
+}
 </script>
 
 <style scoped>
-
 </style>
